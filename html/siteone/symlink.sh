eddc9bf0f643f8c964c33771381696bb1101ce32
Ryan Warner <ryanwarner3@cunet.carleton.ca>
2019-07-09 15:24:31 -0400

    Updates bash script to use an optional parameter

--- html/siteone/symlink.sh
+++ html/siteone/symlink.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+wppath=${1:-"/app/wordpress"}
+
 mkdir wp-content
-ln -s /var/www/multitenant/app/stable ./wp
-ln -s /var/www/multitenant/assets/mu-plugins ./wp-content/mu-plugins
-ln -s /var/www/multitenant/assets/plugins ./wp-content/plugins
-ln -s /var/www/multitenant/assets/themes ./wp-content/themes
-ln -s /var/www/multitenant/config/db-config.php ./db-config.php
-ln -s /var/www/multitenant/config/wp-env.php ./wp-env.php
-ln -s /var/www/multitenant/drop-ins/advanced-cache.php ./wp-content/advanced-cache.php
-ln -s /var/www/multitenant/drop-ins/db.php ./wp-content/db.php
-ln -s /var/www/multitenant/drop-ins/db-error.php ./wp-content/db-error.php
-ln -s /var/www/multitenant/drop-ins/object-cache.php ./wp-content/object-cache.php
+ln -s $wppath/app/stable ./wp
+ln -s $wppath/assets/mu-plugins ./wp-content/mu-plugins
+ln -s $wppath/assets/plugins ./wp-content/plugins
+ln -s $wppath/assets/themes ./wp-content/themes
+ln -s $wppath/config/db-config.php ./db-config.php
+ln -s $wppath/config/wp-env.php ./wp-env.php
+ln -s $wppath/drop-ins/advanced-cache.php ./wp-content/advanced-cache.php
+ln -s $wppath/drop-ins/db.php ./wp-content/db.php
+ln -s $wppath/drop-ins/db-error.php ./wp-content/db-error.php
+ln -s $wppath/drop-ins/object-cache.php ./wp-content/object-cache.php
