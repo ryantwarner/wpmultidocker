2bcbc4e8a8c5d09ea8840110af2a6acbeb3bd14c
Ryan Warner <ryanwarner3@cunet.carleton.ca>
2019-07-18 09:02:11 -0400

    Adds conf, csr, key, and crt for SSL. Adds readme and command to register key.

---
+++ centos/ssl/createcert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+openssl genrsa -out localhost.key 4096 &&
+
+openssl req -new -sha256 \
+    -out localhost.csr \
+    -key localhost.key \
+    -config localhost.conf \
+    -subj "/C=CA/ST=Ontario/L=Ottawa/O=Carleton University/CN=carleton.ca" &&
+
+openssl x509 -req \
+    -sha256 \
+    -days 3650 \
+    -in localhost.csr \
+    -signkey localhost.key \
+    -out localhost.crt \
+    -extensions req_ext \
+    -extfile localhost.conf
+
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain localhost.crt
